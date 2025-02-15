9cefcd837880f7ba59362f7bf4f28d21f54efe08
Kneecrust <kneecrust@gmail.com>
2025-02-15 19:21:00 +0100

    Add tailwind, debugbar & set up first migrations

---
+++ database/migrations/2025_02_15_180735_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('author_id')->constrained()->ondelete('cascade');
+            $table->text('description')->nullable();
+            $table->integer('progress')->default(0);
+            $table->date('date_added')->usecurrent();
+            $table->date('started_reading')->nullable();
+            $table->date('finished_reading')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
